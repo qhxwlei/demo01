ec82b03b9f394aee2fefd47d61e8f2a59ba846ad
qhxwlei <qhxwlei@126.com>
2022-04-04 11:14:29 +0800

    冲突合并在hot-fix分支上面的修改

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,9 @@ public class HelloWorld{
 	public static void main(String[] args){
 
 		System.out.println("Hello World!");
-		System.out.println("hehe~");
+		System.out.println("haha~");
 		System.out.println("在Hot-fix分支上添加的内容");
+		System.out.println("在Hot-fix分支上又添加的内容");
 	}
 
 }
