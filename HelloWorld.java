1726e4613a37b50e53b1864c0665f5da145ac623
qhxwlei <qhxwlei@126.com>
2022-04-04 11:12:14 +0800

    冲突合并在master分支上的修改

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,9 @@ public class HelloWorld{
 	public static void main(String[] args){
 
 		System.out.println("Hello World!");
-		System.out.println("hehe~");
+		System.out.println("heihei~");
 		System.out.println("在Hot-fix分支上添加的内容");
+		System.out.println("在master分支上添加的内容");
 	}
 
 }
