383e5339d865cad2266e736e360bc5807c48266e
wesley <qhxwlei@126.com>
2023-06-07 11:28:44 +0800

    master分支上对HelloWorld.java文件的提交

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,10 @@ public class HelloWorld{
 
 		// 下面一行输出语句是在 hot-fix 分支上新增的
         	System.out.println("hot-fix: content");
+
+		// 下面一行输出语句是在 master 分支上新增的
+        	System.out.println("master: content");
+
 		
 	}
 	
