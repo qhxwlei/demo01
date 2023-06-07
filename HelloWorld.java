ef1391801797832882cf3ff1b0897ba624cf8c6a
wesley <qhxwlei@126.com>
2023-06-07 11:23:12 +0800

    hot-fix:文件HelloWorld.java的第1次提交

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,9 @@ public class HelloWorld{
 	public static void main(String[] args){
 		
 		System.out.println("Hello World!");
+
+		// 下面一行输出语句是在 hot-fix 分支上新增的
+        	System.out.println("hot-fix: content");
 		
 	}
 	
