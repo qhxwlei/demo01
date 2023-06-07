ad73e200718618da1645109a5386d5efe26fcb21
wesley <qhxwlei@126.com>
2023-06-07 11:30:36 +0800

    host-fix分支上对HelloWorld.java文件的第2次提交

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld{
 
 		// 下面一行输出语句是在 hot-fix 分支上新增的
         	System.out.println("hot-fix: content");
+		// 下面一行输出语句还是在 host-fix 分支上新增的
+	        System.out.println("hot-fix: content2");
 		
 	}
 	
